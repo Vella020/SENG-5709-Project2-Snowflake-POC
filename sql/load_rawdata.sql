5d536268dad85d1ee37900d494fdf45dbf4da200
Ryan Kleeberger <rylklee@gmail.com>
2022-03-28 17:51:50 -0500

    fix pattern

--- sql/load_rawdata.sql
+++ sql/load_rawdata.sql
@@ -11,17 +11,17 @@ use schema identifier($schema_name);
 -- copy stage into tables
 
 COPY into customers
-from @s3_customers_stage
-pattern = '.*customers.*.csv';
+from @s3_customers_stage/
+pattern = '.*\.csv';
 
 COPY into employees
-from @s3_employees_stage
-pattern = '.*employees.*.csv';
+from @s3_employees_stage/
+pattern = '.*\.csv';
 
 COPY into products
-from @s3_products_stage
-pattern = '.*products.*.csv';
+from @s3_products_stage/
+pattern = '.*\.csv';
 
 COPY into sales
-from @s3_sales_stage
-pattern = '.*sales.*.csv';
+from @s3_sales_stage/
+pattern = '.*\.csv';
