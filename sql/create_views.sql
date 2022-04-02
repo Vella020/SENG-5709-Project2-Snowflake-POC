c329ba85129c688b6eee8cbf933dccf0b54c486e
Bhavdeep Kumar <bhavsingla@gmail.com>
2022-04-02 16:08:31 -0500

    add first view

---
+++ sql/create_views.sql
@@ -0,0 +1,30 @@
+Set databaseName = 'infiniteloop_sales';
+Set schema_name = 'curated';
+CREATE SCHEMA if NOT EXISTS identifier($schema_name);
+
+Use database identifier($databaseName);
+Use schema identifier($schema_name);
+
+/* Add tempeory view to fetch Customer Id and aggregate total amount of all products purchased by month for 2019*/
+
+create view curated.customerID_monthly_sales_2019_view AS
+select CustomeriD AS "CustomerID",
+to_char(Date, 'yyyy') AS "Year",
+to_char(Date, 'mm') AS "Month",
+SUM (Price) AS "TotalAmount"
+from raw.sales sa, raw.products pr
+where sa.ProductiD = pr.ProductiD
+and to_char(Date, 'yyyy') = 2019
+group by CustomeriD, to_char(Date, 'yyyy'), to_char(Date, 'mm')
+
+/* Add tempeory view to fetch Customer Info and aggregate total amount of all products purchased by month for 2019*/
+
+create view curated.customer_monthly_sales_2019_view AS
+select CustomerID,
+LastName as "CustomerLastName",
+FirstName as "CustomerFirstName",
+Year,
+Month,
+TotalAmount
+from raw.customers cu, curated.customerID_monthly_sales_2019_view vi
+where cu.CustomerID = vi.CustomerID
